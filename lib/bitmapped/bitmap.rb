aeb718af93a6f377e2cce22246fab59b745995f8
Ashley Connor <Ashley.Connor@footballpools.com>
2015-02-24 16:09:57 +0000

    AC : Added terminal-table to pretty print bitmap, added colour command and horizontal command

--- lib/bitmapped/bitmap.rb
+++ lib/bitmapped/bitmap.rb
@@ -1,26 +1,63 @@
+require 'terminal-table'
+
 module Bitmapped
   class Bitmap
+
+    attr_accessor :pixels, :rows, :columns
+
     def command(parsed_input)
-      case parsed_input.first
+      case parsed_input.shift
       when "I"
-        puts "Initiate Command"
+        populate_pixels(parsed_input[0], parsed_input[1])
       when "C"
-        puts "Clear Command"
+        populate_pixels(self.columns, self.rows)
       when "L"
-        puts "Color Command"
+        color_command(parsed_input[0], parsed_input[1], parsed_input[2])
       when "V"
         puts "Vertical Command"
       when "H"
-        puts "Horizontal Command"
+        horizontal_command(parsed_input[0], parsed_input[1], parsed_input[2], parsed_input[3])
       when "F"
         puts "Fill Command"
       when "S"
-        puts "Show Command"
+        pretty_print
       when "X"
         raise Interrupt
       else
         puts "Unknown Command"
       end
     end
+
+    private
+      def populate_pixels(x, y)
+        self.columns = x.to_i
+        self.rows = y.to_i
+        self.pixels = Array.new(self.rows) { Array.new(self.columns) { "0" } }
+      end
+
+      def pretty_print
+        @table = Terminal::Table.new
+        @table.rows = self.pixels
+        puts @table
+      end
+
+      def color_command(x, y, color)
+        self.pixels[y.to_i - 1][x.to_i - 1] = color
+      end
+
+      def vertical_command(column, x, y, color)
+        # vertical command
+      end
+
+      def horizontal_command(x, y, row, color)
+        x = x.to_i - 1
+        y = y.to_i - 1
+        row = row.to_i - 1
+        self.pixels[row][x..y] = Array.new((x..y).size, color)
+      end
+
+      def fill_command
+        # horizontal array
+      end
   end
 end
